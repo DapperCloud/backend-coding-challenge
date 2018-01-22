595f38698c853ba2b9ceb28085e0c59453686990
DapperCloud <quentin.bonnet.work@gmail.com>
2018-01-22 18:49:40 +0100

    Backend now serving front. Gulp dev, then copy static files in src/main/resources/static, then mvn spring-boot:run

---
+++ solution/src/main/java/backendcodingchallenge/SpringBootApplication.java
@@ -0,0 +1,12 @@
+package backendcodingchallenge;
+
+import org.springframework.boot.*;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class SpringBootApplication {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringBootApplication.class, args);
+    }
+
+}
